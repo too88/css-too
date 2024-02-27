7fd68f898e46b7db72379cb0badb02cce8845691
toohuynh <tuhuynh1902@gmail.com>
2024-02-27 21:01:17 +0700

    remove useless code

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -2,16 +2,14 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Header } from './Header';
 
-const meta = {
+const meta: Meta<typeof Header> = {
   title: 'Example/Header',
   component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
   },
-} satisfies Meta<typeof Header>;
+}
 
 export default meta;
 type Story = StoryObj<typeof meta>;
--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -3,21 +3,19 @@ import { within, userEvent, expect } from '@storybook/test';
 
 import { Page } from './Page';
 
-const meta = {
+const meta: Meta<typeof Page> = {
   title: 'Example/Page',
   component: Page,
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
   },
-} satisfies Meta<typeof Page>;
+}
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const LoggedOut: Story = {};
 
-// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
 export const LoggedIn: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
